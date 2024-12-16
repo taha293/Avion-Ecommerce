1e407ed54cce1642da2ba80f820494696b3e39ee
Muhammad Taha <tahahero7890@gmail.com>
2024-12-16 13:12:07 +0500

    .

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -2,15 +2,14 @@
 import Image from "next/image"
 import { useRef } from "react"
 function Banner() {
-    const banner = useRef<HTMLDivElement>(null)
     return (
-        <div className="bg-[#2A254B] px-2 py-1 flex gap-4 text-white items-center justify-between" ref={banner}>
+        <div className="bg-[#2A254B] px-2 py-1 flex gap-4 text-white items-center justify-between">
             <div className="flex gap-2 items-center sm:w-full sm:justify-center">
             <div className="w-4 h-4 flex items-center "><Image src='/assets/Delivery2.png' alt="" width={14} height={11} className="max-w-none" /></div>
                  <p className="text-[14px]">Free delivery on all orders over £50 with code easter checkout</p>           
             </div>
             <div className="w-6 h-6 flex justify-center items-center">
-            <Image className="cursor-pointer" onClick={()=>banner.current.style.display = 'none'} src='/assets/Close.png' alt="searchbar" width={12} height={12} />
+            <Image className="cursor-pointer" src='/assets/Close.png' alt="searchbar" width={12} height={12} />
             </div>
         </div>
     )
