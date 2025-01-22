4d068622a1ca64ac4d1bde7c6b6cf8b0ba27ab84
Muhammad Taha <tahahero7890@gmail.com>
2025-01-22 15:22:58 +0500

    Update page.tsx

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Features from "@/components/features"
 import SignUp from "@/components/signup"
 import ProductDetails from "@/components/productdetails"
 import ShowProducts from "@/components/showproducts"
+import { NextPage } from "next"
 
 interface props {
   params : {
@@ -11,7 +12,7 @@ interface props {
   }
 }
 
-function Products({params}:props) {
+const Products: NextPage<props> = async ({ params }) => {
   return (
     <div>
       <NAVBAR />
